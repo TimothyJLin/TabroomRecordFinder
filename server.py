31de4a0477109eec6761b758ce1dea3c42c910b3
Glitch (cord-pointy-bandicoot) <none>
2024-08-29 22:29:00 +0000

    🚁💡 Checkpoint ./server.py:69963853/70 ./templates/index.html:69963853/242

--- server.py
+++ server.py
@@ -7,6 +7,9 @@ app = Flask(__name__)
 def home():
     return render_template('index.html')
 
+def printTest():
+  print("hello world from server.py")
+  
 
 if __name__ == '__main__':
     app.run(debug=True)
