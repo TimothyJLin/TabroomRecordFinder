3cc91856de0437a8c9929c0c28733cad803fb07e
Glitch (cord-pointy-bandicoot) <none>
2024-08-30 08:30:41 +0000

    🍠🔖 Checkpoint ./server.py:69963853/90

--- server.py
+++ server.py
@@ -11,18 +11,18 @@ def printTest():
   print("hello world from server.py")
 
   
-  app.get("/html", (request, response) => {
-  try {
-    fetch(request.query.url)
-      .then(res => res.text())
-      .then(text => {
-        response.send(text);
-      });
-  } catch(e) {
-    response.status(400).json(
-        { message: "Something went wrong." + e}
-      );
-  }
+app.get("/html", (request, response) => {
+try {
+  fetch(request.query.url)
+    .then(res => res.text())
+    .then(text => {
+      response.send(text);
+    });
+} catch(e) {
+  response.status(400).json(
+      { message: "Something went wrong." + e}
+    );
+}
 });  
 
 if __name__ == '__main__':
