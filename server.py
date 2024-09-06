d5423ee917efac3708712f404e7d214130a3c380
Glitch (cord-pointy-bandicoot) <none>
2024-09-06 22:08:11 +0000

    ✂️😄 Checkpoint ./server.py:69963853/680

--- server.py
+++ server.py
@@ -9,28 +9,27 @@ def home():
 
 @app.route('/sqlBackend.py', methods=['GET', 'POST'])
 def handle_request():
-    if request.method == 'GET':
-        # Handle GET requests
-        data = {'message': 'Hello from the GET endpoint'}
-        return jsonify(data)
-    if request.method == 'POST':
-      if request.is_json:
-        data = request.get_json() 
-        if data is None:
-          return jsonify({'error': 'Invalid JSON data'}), 400      
-        #return jsonify("POST REQUEST RECIEVED STONKS")
+  if request.method == 'POST':
+    
+    if request.is_json:
+      data = request.get() 
+      return data
+
+      if data is None:
+        return jsonify({'error': 'Invalid JSON data'}), 400      
+      #return jsonify("POST REQUEST RECIEVED STONKS")
 
 
-        #data=request.get['inputData']
-        #name=data['name']
-        #school=data['school']
-        #return jsonify("school: "+school)
-        result=sqlBackend.findDebater("name", "school")
-        return result
-      else:
-          return jsonify({'error': 'Unsupported method'}), 405 
-      
-      
+      #data=request.get['inputData']
+      #name=data['name']
+      #school=data['school']
+      #return jsonify("school: "+school)
+      result=sqlBackend.findDebater("name", "school")
+      return result
+    else:
+        return jsonify({'error': 'Unsupported method'}), 405 
+
+
 if __name__ == '__main__':
     app.run(debug=True)
     
