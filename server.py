f55e9bfe17bcd7642adfe02ddb754774e68a5e0d
Glitch (cord-pointy-bandicoot) <none>
2024-09-06 09:52:33 +0000

    🌻🌍 Checkpoint ./server.py:69963853/212

--- server.py
+++ server.py
@@ -18,11 +18,13 @@ def handle_request():
       if data is None:
         return jsonify({'error': 'Invalid JSON data'}), 400      
       #return jsonify("POST REQUEST RECIEVED STONKS")
-      data=request.get['inputData']
-      name=data['name']
-      school=data['school']
-      return jsonify("school: "+school)
-      result=sqlBackend.findDebater(name, school)
+      
+      
+      #data=request.get['inputData']
+      #name=data['name']
+      #school=data['school']
+      #return jsonify("school: "+school)
+      result=sqlBackend.findDebater("name", "school")
       return result
     else:
         return jsonify({'error': 'Unsupported method'}), 405 
